58561279fe17afdc61862e8ff8cf05e1e54b90b5
Gabriel Henton <59454894+gsauce7@users.noreply.github.com>
2022-07-24 14:42:45 -0700

    all is working with my first error handling middleware just before adding redux

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
@@ -16,6 +17,10 @@ app.get('/', (req, res) => {
 
 
 app.use('/api/products', productRoutes)
+
+//bring in the middleware
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5001
 
